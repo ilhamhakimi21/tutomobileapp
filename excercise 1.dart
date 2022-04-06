a2dbc29e0537c2cd97d0b6d702ba0fb1e95ba988
ilhamhakimi21 <75355353+ilhamhakimi21@users.noreply.github.com>
2022-04-06 18:00:53 +0800

    commit on 'tutomobileapp'

---
+++ excercise 1.dart
@@ -0,0 +1,16 @@
+void main(){
+
+var  firstname = "Muhammad Ilham Hakimi";
+dynamic lastname = "Mohamad Nkzam";
+
+int year = 2022;
+int birthyear = 2000;
+int age = 2022 - birthyear;
+
+double cgpa = 3.5;
+String address = "Mahallah Ali Blok E, IIUM, Gombak";
+
+print("My name is $firstname $lastname. I am $age this year. I am staying at $address. My CGPA is $cgpa");
+
+}
+
